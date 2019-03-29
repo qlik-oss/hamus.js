92bd9a6d7e16f2ccc1e35c55f1f5a728f24cbae1
Helene Rigner <hvs@qlik.com>
2019-03-29 14:08:03 +0100

    Letting puppeteer start the dev server

--- test/e2e/example.test.js
+++ test/e2e/example.test.js
@@ -3,14 +3,15 @@ import puppeteer from 'puppeteer';
 let browser;
 let page;
 
-beforeAll(async () => {
-  browser = await puppeteer.launch();
-  page = await browser.newPage();
-});
-
 describe('e2e for example app', () => {
-  test('data is fetched and chart is rendered', async () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    jest.setTimeout(35000);
     await page.goto('http://localhost:1234/');
+  });
+
+  test('data is fetched and chart is rendered', async () => {
     const html = await page.$eval('.chart', e => e.innerHTML);
     expect(html).toContain('Fetching app');
     // check if chart has been rendered
