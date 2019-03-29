f64ddc172d46cd088fa63559a5169555c8c18d95
Helene Rigner <hvs@qlik.com>
2019-03-29 14:22:18 +0100

    debugging cci

--- test/e2e/example.test.js
+++ test/e2e/example.test.js
@@ -5,22 +5,35 @@ let page;
 
 describe('e2e for example app', () => {
   beforeAll(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-    jest.setTimeout(35000);
-    await page.goto('http://localhost:1234/');
+    try {
+      browser = await puppeteer.launch();
+      page = await browser.newPage();
+      jest.setTimeout(35000);
+      await page.goto('http://localhost:1234/');
+    } catch (error) {
+      console.log(error);
+    }
   });
 
   test('data is fetched and chart is rendered', async () => {
-    const html = await page.$eval('.chart', e => e.innerHTML);
-    expect(html).toContain('Fetching app');
-    // check if chart has been rendered
-    await page.waitFor(2000);
-    const bar = await page.$('[data-label="Django Unchained"]');
-    expect(bar).not.toBeNull();
+    try {
+      jest.setTimeout(35000);
+      const html = await page.$eval('.chart', e => e.innerHTML);
+      expect(html).toContain('Fetching app');
+      // check if chart has been rendered
+      await page.waitFor(2000);
+      const bar = await page.$('[data-label="Django Unchained"]');
+      expect(bar).not.toBeNull();
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
 
-afterAll(() => {
-  browser.close();
+afterAll(async () => {
+  try {
+    await browser.close();
+  } catch (error) {
+    console.log(error);
+  }
 });
